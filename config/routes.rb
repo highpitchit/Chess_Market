5a7869acc71dd32109c9b0ac354047ece5ca85e8
highpitchit <ameliafrancesnunn@gmail.com>
2020-05-18 13:11:17 +1000

    fixed routing error

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,17 @@ Rails.application.routes.draw do
   
 
 
-  get 'pages/index'
-  get 'pages/show'
-  get 'pages/new'
-  get 'pages/edit'
-  get 'pages/create'
-  get 'pages/update'
-  get 'pages/destroy'
-  resources :advertises 
+  get '/pages/index'
+  get '/pages/show'
+  #get '/users/:id', to: "users#show", as: "profile"
+  # patch '/users/:id', to: "users#update"
+
+  # get 'pages/new'
+  # get 'pages/edit'
+  # get 'pages/create'
+  # get 'pages/update'
+  # get 'pages/destroy'
+  
   resources :forums do
     post '/comments', to: "comments#create"
     patch '/comments/:comment_id', to: "comments#update"
@@ -17,9 +20,9 @@ Rails.application.routes.draw do
     delete '/comments/:comment_id', to: "comments#destroy"
   end
  
-  
+  resources :advertises 
   devise_for :users
   resources :users
-  root to: "users#index"
+  root to: "users#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
