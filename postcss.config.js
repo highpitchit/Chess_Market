7ca76fd4bbd1a667aab7c69fd003e0be08aa4049
highpitchit <ameliafrancesnunn@gmail.com>
2020-05-20 09:03:53 +1000

    change tailwind settings

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   plugins: [
     require('postcss-import'),
-    require('tailwindcss')('./app/javascript/css/tailwind.js'),
+    require('tailwindcss'),
     require('autoprefixer'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
