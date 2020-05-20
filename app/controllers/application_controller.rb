6a39733b449a26ab151df6c2885ca9decb20c1f4
highpitchit <ameliafrancesnunn@gmail.com>
2020-05-20 23:19:02 +1000

    tidying up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
-    before_action :authenticate_user!, except: [:home]
-    
+    before_action :authenticate_user!, except: [:home]  
+
     def after_sign_in_path_for(resource)
-        return edit_user_path(current_user.id)
+        return root_path(current_user.id)
     end
 
     def after_sign_up_path_for(resource)
--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,12 @@ Rails.application.routes.draw do
 
   resources :forums do
     post '/comments', to: "comments#create"
-    # patch '/comments/:comment_id', to: "comments#update"
-    # get '/comments/:comment_id', to: "comments#show"
     delete '/comments/:comment_id', to: "comments#destroy"
   end
  
   resources :advertises 
   devise_for :users
-  resources :users
+  resources :users, except: [:show]
   root to: "splashes#home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
